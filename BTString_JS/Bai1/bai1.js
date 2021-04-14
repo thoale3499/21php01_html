7a87622ab73f521ffc290f39ddfc5f5b2736887e
thoale3499 <lethikimthoa341999@gmail.com>
2021-04-14 18:40:25 +0700

    BTVN

--- BTString_JS/Bai1/bai1.js
+++ BTString_JS/Bai1/bai1.js
@@ -4,10 +4,10 @@ document.write('<table border = "0">');
 for ( i = 0; i<n; i++){ //Vòng for in theo dòg
 	for (j=n-1;j>i;j--){
 		document.write('<td>');
-		document.write('&nbsp;');
+		document.write(' ');
 		document.write('</td>');
 	}
-	for (j=1;j<=i*2-1;j++){//In ra kí tự * tăng dần theo số lẻ
+	for (j=1;j<=i*2-1;j++){
 		document.write('<td>');
 		document.write('*');
 		document.write('</td>');
